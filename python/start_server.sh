87bbcf8976ef76c6543dacbb6aace9c827bbddfe
robot144 <robot144@protonmail.com>
2023-11-28 20:41:01 +0000

    Keep processes running after end of user session.

--- python/start_server.sh
+++ python/start_server.sh
@@ -4,6 +4,12 @@
 # detect docker or podman
 . ../detect_podman_or_docker.sh
 
-# basic nginx as a first start
+# run container in this folder
 # $CONTAINERRUNNER run -it --rm --name python_repl ipython
-$CONTAINERRUNNER run -it --rm -d -p 8080:8765 --name python_app_1 python_jupyter
+export DEBUG=false
+if [ $DEBUG == "true" ];then
+   $CONTAINERRUNNER run -it --rm -d -p 8080:8765 --name python_app_1 python_jupyter
+else
+   loginctl enable-linger # keep podman process alive after logout
+   $CONTAINERRUNNER run --rm -d -p 8080:8765 --name python_app_1 python_jupyter
+fi
